114d038f8ef3d300b24529741aa2703d17dd9af1
leehyunju475 <jiji812@daum.net>
2016-10-15 15:10:49 +0900

    day1 파이썬 코드 실습

---
+++ day1/turtle1.py
@@ -0,0 +1,24 @@
+import turtle
+
+turtle.shape("turtle")
+turtle.speed(2)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(140)
+turtle.right(90)
+turtle.forward(140)
+turtle.right(90)
+turtle.forward(180)
+turtle.right(90)
+turtle.forward(180)
+turtle.right(90)
+turtle.forward(180)
+turtle.right(90)
+turtle.forward(40)
+
+
+
---
+++ day1/삼각형.py
@@ -0,0 +1,18 @@
+import turtle
+
+turtle.shape("turtle")
+
+turtle.speed(5)
+
+turtle.penup()
+turtle.backward(100)
+turtle.pendown()
+
+turtle.forward(200)
+turtle.right(120)
+turtle.forward(200)
+turtle.right(120)
+turtle.forward(200)
+
+turtle.right(180)
+turtle.circle(100)
